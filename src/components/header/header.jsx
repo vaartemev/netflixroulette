241c5237b08dd01557a22565b1ee0d6788aa9756
Vladimir Artemev <im.artemev@gmail.com>
2019-08-18 16:48:01 +0300

    Some fix for styles

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,9 +7,12 @@ import { getMoviesBySearchQuery } from '../../actions';
 import './header.scss';
 
 export const Header = () => {
-  const searchValue = useSelector(state => state.movie.searchValue);
-  const searchFilter = useSelector(state => state.movie.searchBy);
-  const sortFilter = useSelector(state => state.movie.sortBy);
+  const { searchValue, searchFilter, sortFilter } = useSelector(state => ({
+    searchValue: state.movie.searchValue,
+    searchFilter: state.movie.searchBy,
+    sortFilter: state.movie.sortBy
+  }));
+
   const dispatch = useDispatch();
 
   const handleOnKeyPress = event => {
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,28 +1,40 @@
 import axios from 'axios';
-import { stringify } from 'query-string';
-import {GET_MOVIE_DETAILS_BY_ID} from '../constants'
-import { baseUrl } from '../constants';
+import {
+	stringify
+} from 'query-string';
+import {
+	GET_MOVIE_DETAILS_BY_ID
+} from '../constants'
+import {
+	baseUrl
+} from '../constants';
 
-export const fetchMovies = ({type, payload}) => {
-  let url = baseUrl;
-  switch (type) {
-    case GET_MOVIE_DETAILS_BY_ID: {
-	  url = `${url}/${payload.id}`;
-      break;
+export const fetchMovies = ({
+	type,
+	payload
+}) => {
+	let url = baseUrl;
+	switch (type) {
+		case GET_MOVIE_DETAILS_BY_ID: {
+			url = `${url}/${payload.id}`;
+			break;
+		}
+		default:
+			url += `?${stringify(payload)}`
+			break
 	}
-    default:
-	   url += `?${stringify(payload)}`
-      break
-  }
-  return axios(url).then(response => {
-    return response.data;
-  });
+	return axios({
+		method: 'get',
+		url: url
+	}).then(response => {
+		return response.data;
+	});
 };
 
 export const getFilms = (action) => {
-  try {
-    return fetchMovies(action);
-  } catch (err) {
-    console.err(err);
-  }
+	try {
+		return fetchMovies(action);
+	} catch (err) {
+		console.err(err);
+	}
 };
