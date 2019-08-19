20e58bc0701f797a17c174f37d625a8459f4df71
Vladimir Artemev <Vladimir_Artemev@epam.com>
2019-08-19 14:45:36 +0300

    Add propTypes for Logo

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './logo.scss';
 
 export const Logo = ({ text }) => {
   return <div className="logo"> {text} </div>;
+};
+
+Logo.propTypes = {
+  text: PropTypes.string
 };
