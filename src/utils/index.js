a60166ddfbad29df2b50544699aa6956ecddcc08
Vladimir Artemev <Vladimir_Artemev@epam.com>
2019-08-28 15:21:52 +0300

    Some graph

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,27 +1,46 @@
 import 'regenerator-runtime/runtime';
 import axios from 'axios';
 import { stringify } from 'query-string';
+import gql from 'graphql-tag';
+import { client } from '../client';
 
-const instance = axios.create({
-  baseURL: 'http://react-cdp-api.herokuapp.com/movies',
-});
+const fetchMovieByIdQuery = gql`
+  query($id: String!) {
+    movie(id: $id) @rest(type: "Movie", path: "/{args.id}") {
+      id
+      title
+      release_date
+      poster_path
+      overview
+      genres
+      runtime
+    }
+  }
+`;
 
-export const fetchMovies = async params => {
-  const movies = await instance
-    .get(`?${stringify(params)}`)
-    .then(response => {
-      return response.data;
-    })
-    .catch(error => console.error(error));
-  return movies;
-};
+// const fetchMoviesQuery = gql`
+//   query($id: String!) {
+//     movie(id: $id) @rest(type: "Movie", path: "/{args.id}") {
+//       id
+//       title
+//       release_date
+//       poster_path
+//       overview
+//       genres
+//       runtime
+//     }
+//   }
+// `;
+
+// export const fetchMovies = async params => {
+//   const searchParams = `?${stringify(params)}`;
+// };
 
 export const fetchMovieById = async params => {
-  const movie = await instance
-    .get(`/${params}`)
-    .then(response => {
-      return response.data;
-    })
-    .catch(error => console.error(error));
-  return movie;
+  const movie = await client.query({
+    query: fetchMovieByIdQuery,
+    variables: { id: String(params) },
+  });
+  console.log(movie.data.movie);
+  return movie.data.movie;
 };
