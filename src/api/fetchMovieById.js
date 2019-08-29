eaaa03696091a10e5b3e9501c6539eff01680aac
Vladimir Artemev <Vladimir_Artemev@epam.com>
2019-08-29 13:47:51 +0300

    Fix for api

--- src/api/fetchMovieById.js
+++ src/api/fetchMovieById.js
@@ -1,8 +1,8 @@
 import 'regenerator-runtime/runtime';
 import axios from 'axios';
 
-export const fetchMovieById = async params => {
-  const res = await axios.get(`/${params}`);
+export const fetchMovieById = async id => {
+  const res = await axios.get(`/${id}`);
   const movie = await res.data;
   return movie;
 };
